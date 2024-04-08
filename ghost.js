e815ef3deef586a6c54b5110724e1f7c226f50c8
william <wilsher987@gmail.com>
2024-04-07 21:51:08 -0500

    mapdirection

--- ghost.js
+++ ghost.js
@@ -28,5 +28,199 @@ class Ghost {
         }
         return false;
     }
+    changeRandomDirection(){
+        let addition = 1;
+        this.randomTargetIndex += addition;
+        this.randomTargetIndex =this.randomTargetIndex % 4;
+    }
+    moveProcess(){
+        if (this.isInRange()) {
+            this.target = pacman;
+        } else {
+            this.target = randomTargetsForGhosts[this.randomTargetIndex];
+        }
+        this.changeRandomDirectionPossible();
+        this.moveForwards();
+        if (this.checkCollisions()) {
+            this.moveBackwards();
+            return;
+        }
+    }
+    moveBackwards(){
+        switch (this.direction) {
+            case 4: //Rigth
+            this.x -= this.speed;
+            break;
+            case 3: //Up
+            this.y -= this.speed;
+            break;
+            case 2: //Left
+            this.x -= this.speed;
+            break;
+            case 1: //Bottom
+            this.y -= this.speed;
+            break;
+        }
+    }
+
+    moveForwards() {
+        switch (this.direction) {
+            case 4: //Rigth
+            this.x -= this.speed;
+            break;
+            case 3: //Up
+            this.y -= this.speed;
+            break;
+            case 2: //Left
+            this.x -= this.speed;
+            break;
+            case 1: //Bottom
+            this.y -= this.speed;
+            break;
+        }
+    }
+    checkCollisions() {
+        let isCollied = false
+        if (
+            map[parseInt(this.y/oneBlockSize)][
+                parseInt(this.x/oneBlockSize)
+            ] == 1 ||
+            map[parseInt(this.y/oneBlockSize + 0.9999)][
+                parseInt(this.x/oneBlockSize)
+            ] == 1 ||
+            map[parseInt(this.y/oneBlockSize)][
+                parseInt(this.x/oneBlockSize + 0.9999)
+            ] == 1 ||
+            map[parseInt(this.y/oneBlockSize + 0.9999)][
+                parseInt(this.x/oneBlockSize + 0.9999)
+            ] == 1
+        ) {
+            isCollied = true;
+        }
+        return isCollied;
+    }
+    changeDirectionIfPossible(){
+        let tempDirection = this.direction;
+        this.direction = this.calculateNewDirection(
+            map,
+            parseInt(this.target.x / oneBlockSize),
+            parseInt(this.target.y / oneBlockSize)
+        );
+        if(typeof this.direction == "undefined"){
+            this.direction = tempDirection;
+            return;
+        }
+
+        if(
+            this.getMapY() != this.getMapYRightSide()&&
+            (this.direction == DIRECTION_LEFT ||
+                this.direction == DIRECTION_RIGHT)
+        ){
+            this.direction = DIRECTION_UP;
+        }
+        if ( 
+            this.getMapX() != this.getMapXRightSide() &&
+            this.direction == DIRECTION_UP
+        ){
+            this.direction = DIRECTION_LEFT;
+        }
+        this.moveForwards();
+        if (this.checkCollisions()){
+            this.moveBackwards();
+            this.direction = tempDirection;
+        }else{
+            this.moveBackwards();
+        }
+        console.log(this.direction);
+    }
+    calculateNewDirection(map, destX, destY) {
+        let mp = [];
+        for (let i = 0 ; i<map.length; i++){
+            mp[i] = map[i].slice();
+        }
+        let queue = [
+            {
+                x: this.getMapX(),
+                y: this.getMapY(),
+                rightX: this.getMapXRightSide(),
+                rightY: this.getMapYRightSide(),
+                moves: [],
+            },
+        ];
+        while (queue.length > 0){
+            let poped = queue.shift();
+            if (poped.x == destX && poped.y == destY){
+                return poped.moves[0];
+            }else{
+                mp[poped.y][poped.x]=1
+                let neighborList = this.addNeighbors(poped, mp);
+                for (let i=0; i<neighborList.length; i++){
+                    queue.push(neighborList[i])
+                }
+            }
+        }
+        return 1; //ghost direction
+    }
+    addNeighbors(poped, mp){
+        let queue = [];
+        let numOfRows=mp.length;
+        let numOfColumns=mp[0].length;
+        if(
+            poped.x - 1 >= 0 &&
+            poped.x - 1 < numOfRows &&
+            mp[poped.y][poped.x - 1] !=1
+        ){
+            let tempMoves = poped.moves.slice();
+            tempMoves.push(DIRECTION_LEFT);
+            queue.push({x: poped.x - 1, y: poped.y, moves: tempMoves});
+        }
+        if(
+            poped.x + 1 >= 0 &&
+            poped.x - 1 < numOfRows &&
+            mp[poped.y][poped.x + 1] !=1
+        ){
+            let tempMoves = poped.moves.slice();
+            tempMoves.push(DIRECTION_RIGHT);
+            queue.push({X: poped.x + 1, y: poped.y, moves: tempMoves});
+        }
+        if(
+            poped.y - 1 >= 0 &&
+            poped.y - 1 < numOfColumns &&
+            mp[poped.y - 1][poped.x]!=1
+        ){
+            let tempMoves = poped.moves.slice();
+            tempMoves.push(DIRECTION_UP);
+            queue.push({x: poped.x, y: poped.y - 1, moves: tempMoves});
+        }
+        if(
+            poped.x + 1 >= 0 &&
+            poped.x + 1 < numOfRows &&
+            mp[poped.y + 1][poped.x] !=1
+        ){
+            let tempMoves = poped.moves.slice();
+            tempMoves.push(DIRECTION_BOTTOM);
+            queue.push({X: poped.x, y: poped.y + 1, moves: tempMoves});  
+        }
+        return queue;
+    }
+    getMapX(){
+        let mapX = parseInt(this.x/oneBlockSize);
+        return mapX;
+    }
+    getMapY(){
+        let mapY = parseInt(this.y/oneBlockSize);
+        return mapY;
+    }
+    getMapXRightSide(){
+        let mapX = parseInt(this.x*0.99 + oneBlockSize)/ oneBlockSize;
+        return mapX;
+    }
+    getMapYRightSide(){
+        let getMapY = parseInt((this.y*0.99 + oneBlockSize)/ oneBlockSize);
+        return mapY;
+    }
+    changeAnimation(){
+        this.currentFrame = this.currentFrame == this.frameCount ? 1 : this.currentFrame + 1;
+    }
     
 }
